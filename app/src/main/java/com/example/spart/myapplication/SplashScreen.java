10f575fd3c1f6d849ee50f8f0a16c564de34a22e
spartam72 <spartam72@yahoo.fr>
2019-05-04 18:01:16 +0200

    splash

--- app/src/main/java/com/example/spart/myapplication/SplashScreen.java
+++ app/src/main/java/com/example/spart/myapplication/SplashScreen.java
@@ -7,6 +7,9 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 
+/**
+ * splash_screen
+ */
 public class SplashScreen extends AppCompatActivity {
 
     @Override
